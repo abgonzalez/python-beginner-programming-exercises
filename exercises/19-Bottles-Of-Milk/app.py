43bb70aa5d01824e732ae0742b647ea34ac1c58c
abgonzalez <ana.gonzalez.villalba@gmail.com>
2025-12-23 13:40:44 +0000

    Update the answers of the exercises

--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,1 +1,16 @@
 # ✅↓ Write your code here ↓✅
+def number_of_bottles():
+    for i in range(99,0, -1):
+        if i == 1 :
+            print(i , " bottle of milk on the wall, ", i, " bottle of milk.")
+            print(" Take one down and pass it around, no more bottles of milk on the wall.")
+        elif i == 2 :
+            print(i , " bottles of milk on the wall, ", i, " bottles of milk.")
+            print("Take one down and pass it around, ", i-1, "bottle of milk on the wall.")
+        else:    
+            print(i , " bottles of milk on the wall, ", i, " bottles of milk.")
+            print("Take one down and pass it around, ", i-1, "bottles of milk on the wall.")
+    print("No more bottles of milk on the wall, no more bottles of milk.")
+    print("Go to the store and buy some more, 99 bottles of milk on the wall.")
+
+number_of_bottles()
