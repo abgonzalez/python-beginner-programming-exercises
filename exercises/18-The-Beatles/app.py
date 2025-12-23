52dd8a8190111195478db873388ed2ec9e728a12
abgonzalez <ana.gonzalez.villalba@gmail.com>
2025-12-23 15:31:57 +0000

    Exercises done

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,16 @@
 # ✅↓ Write your code here ↓✅
+# ✅↓ Write your code here ↓✅
 
+def sing():
+    lyrics_string = ''
+    for i in range(4):
+        lyrics = 'let it be,\n'
+        lyrics_string += lyrics
+    lyrics_string += "there will be an answer,\n"
+    for i in range(5):
+        lyrics = 'let it be,\n'
+        lyrics_string += lyrics
+    lyrics_string += "whisper words of wisdom, let it be"
+    return lyrics_string
+
+print(sing())
